8f4c11bbc5ab90d9e6df7566c0da7ef36f6ee422
kmsheng <kmsheng@pixnet.tw>
2013-02-05 18:15:59 +0800

    define angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,14 @@
-define([], function() {
+define(['angular'], function(angular) {
     return angular.module('pixnet', [])
-   .config(['$routeProvider', function($routeProvider){
-        $routeProvider.when('/', {
-            templateUrl: 'views/index.html'
-        });
-        $routeProvider.when('/test', {
-            templateUrl: 'views/test.html'
-        });
-        $routeProvider.otherwise({
-            redirectTo: '404'
-        });
-   }]);
+       .config(['$routeProvider', function($routeProvider){
+            $routeProvider.when('/', {
+                templateUrl: 'views/index.html'
+            });
+            $routeProvider.when('/test', {
+                templateUrl: 'views/test.html'
+            });
+            $routeProvider.otherwise({
+                redirectTo: '404'
+            });
+       }]);
 });
