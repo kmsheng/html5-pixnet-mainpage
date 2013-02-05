8a249ee6ecb81ee337ba35a04cbf5b1a3a57e844
kmsheng <kmsheng@pixnet.tw>
2013-02-05 16:55:34 +0800

    add angular route example

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,14 @@
-define(['angular'], function(angular) {
-    return angular.module('pixnet', []);
+define([], function() {
+    return angular.module('pixnet', [])
+   .config(['$routeProvider', function($routeProvider){
+        $routeProvider.when('/', {
+            templateUrl: 'views/index.html'
+        });
+        $routeProvider.when('/test', {
+            templateUrl: 'views/test.html'
+        });
+        $routeProvider.otherwise({
+            redirectTo: '404'
+        });
+   }]);
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,32 @@
 require.config({
-  shim: {
-    'angular': {exports: 'angular'},
-    'angular-resource': {deps:['angular']},
-    'lodash': {exports: '_'}
-  },
-
-  paths: {
-    hm: 'vendor/hm',
-    esprima: 'vendor/esprima',
-    jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min',
-    lodash: 'vendor/lodash.min',
-    modernizr: 'vendor/modernizr.min',
-    angular: '//ajax.googleapis.com/ajax/libs/angularjs/1.0.1/angular.min'
-  }
+    shim: {
+        'angular': {exports: 'angular'},
+        'angular-resource': {deps:['angular']},
+        'lodash': {exports: '_'}
+      },
+    paths: {
+        hm: 'vendor/hm',
+        esprima: 'vendor/esprima',
+        jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min',
+        lodash: 'vendor/lodash.min',
+        modernizr: 'vendor/modernizr.min',
+        angular: '//ajax.googleapis.com/ajax/libs/angularjs/1.0.1/angular.min'
+    },
+    priority: [
+        'angular'
+    ],
+    urlArgs: 'v=1.1'
 });
 
 require([
   'angular',
   'app',
   'controllers/controllers',
+  'filters/filters',
+  'directives/directives'
 ], function(angular, app){
 
     angular.element(document).ready(function(){
         angular.bootstrap(document, ['pixnet']);
     });
-
 });
